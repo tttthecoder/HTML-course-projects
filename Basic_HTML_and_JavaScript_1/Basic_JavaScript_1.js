36745c692df81cd1d9f73f009f8676e2c28aa0eb
tttthecoder <trungtintran1905@gmail.com>
2023-07-05 00:18:37 -0600

    Added a folder named 'Basic_HTML_and_JavaScript_1'
    Added a folder named 'Basic_HTML_and_JavaScript_1'. Made some HTML files that demonstrated the concepts learned!

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+// alert('this is javascript example ')
+function My_first_function() {
+    var String = 'Kiss me, I\'m Irish'
+    var result = String.fontcolor('green')
+    document.getElementById('irish').innerHTML = result
+}
